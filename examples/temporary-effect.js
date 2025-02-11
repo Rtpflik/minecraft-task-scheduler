3f088a3ac6dcb6e016cc27538ad8321c795d5538
Rtpflik <129345256+Rtpflik@users.noreply.github.com>
2025-02-10 19:04:38 -0600

    Create temporary-effect.js

---
+++ examples/temporary-effect.js
@@ -0,0 +1,13 @@
+import { world } from "@minecraft/server";
+import { TaskScheduler } from "./TaskScheduler";
+
+world.events.playerJoin.subscribe((event) => {
+    const player = event.player;
+
+    console.log(`${player.name} has received a temporary effect!`);
+
+    // Simulate applying an effect
+    TaskScheduler.schedule(() => {
+        console.log(`${player.name}'s effect has worn off.`);
+    }, 400); // 20 seconds 
+});
